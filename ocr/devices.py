ecba809e2000d1369f4b1ab6c06ab043b763d5d4
Robert Xiao <brx@cs.cmu.edu>
2015-01-30 03:37:21 -0500

    Profile for a OnePlus One phone

--- ocr/devices.py
+++ ocr/devices.py
@@ -12,4 +12,5 @@ CONFIGS = {
     # sw,sh: screen width and height (set automatically)
 
     'LGE Nexus 5': Namespace(sw=1080, sh=1920, x0=141, y0=577,  w=144, h=112,  dx=219, dy=292,  tx=310, ty=128, tw=460, th=188),
+    'OnePlus A0001': Namespace(sw=1080, sh=1920, x0=182, y0=608,  w=112, h=96,  dx=202.5, dy=270,  tx=330, ty=172, tw=440, th=174),
 }
